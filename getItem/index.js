f9406cff03b6a006d70851283a80847823736988
Natalie Hong <nataliehsm@gmail.com>
2020-01-01 00:27:34 +0800

    completed getItem

--- getItem/index.js
+++ getItem/index.js
@@ -4,23 +4,54 @@ const dbClient = new AWS.DynamoDB.DocumentClient({region:'ap-southeast-1'});
 
 exports.handler = async (event) => {
     
-    var params = {
-        TableName: 'Item_table',
-        Key:{
-            'id': event.queryStringParameters.id
-        }
-    };
+    const params = {
+    TableName: 'Item_table',
+        FilterExpression: '#key = :key',
+        ExpressionAttributeNames: {
+            '#key': 'key',
+        },
+        ExpressionAttributeValues: {
+            ':key': event.queryStringParameters.key,
+        },
+    }; 
     
     
     return new Promise((resolve, reject) =>{
-        dbClient.get(params, function(err, data){
-            if(err){
-                reject(err);
-            }
-            else{
-                return resolve({statusCode: 200, body: JSON.stringify(data)});
-            }
+        scanTable(params, event)
+            .then(result => {
+                if(result){
+                    return resolve({statusCode: 200, body: JSON.stringify(result)});
+                }
+                else {
+                    return resolve({statusCode: 404});
+                }
         });
     });
+    
 };
 
+
+var scanTable = async (params, event) => {
+    let items = await dbClient.scan(params).promise();
+    let itemList = items.Items;
+    var result;
+    if (event.queryStringParameters.timestamp){
+        itemList.forEach(item=>{
+            if(item.timestamp == event.queryStringParameters.timestamp){
+                result = item;
+            }
+        })
+    }
+    else {
+        //Sort and return latest
+        itemList.sort((function (a, b) { 
+                              return new Date(b.timestamp) - new Date(a.timestamp);
+                            }));
+        result = itemList[0];
+    }
+    if (result) {
+        delete result.timestamp; //remove timestamp
+        delete result.id; //remove id
+    }
+    return result;
+};
